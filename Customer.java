ffad07987c4cfeff2712d007e9752563cad30014
Jana-Alothman <445200267@student.ksu.edu.sa>
2025-11-05 13:00:18 +0300

    Add files via upload

--- Customer.java
+++ Customer.java
@@ -2,107 +2,94 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package csc212;
 
-
+/**
+ *
+ * @author Jalot
+ */
 public class Customer {
-    private int customerId;
-    private String name;
-    private String email;
-    private LinkedList<Integer> orders = new LinkedList<Integer>();
-
-
-    public Customer() {
-        this.customerId = 0;
-        this.name = "";
-        this.email = "";
+    
+    private int customerId ;
+    private String name ;
+    private String email ;
+    private LinkedList<Integer> orders = new LinkedList<Integer>() ;
+    
+    public Customer(){
+        this.customerId = 0 ;
+        this.name = "" ;
+        this.email = "" ;
     }
-
-    public Customer(int customerId, String name, String email) {
-        this.customerId = customerId;
-        this.name = name;
-        this.email = email;
+    
+    public Customer(int customerId , String name , String email){
+        this.customerId = customerId ;
+        this.name = name ;
+        this.email = email ;
     }
-
-    public int getCustomerId() {
-        return customerId;
+    
+    public int getCustomerId(){
+        return customerId ;
     }
-
     public void setCustomerId(int customerId) {
-        this.customerId = customerId;
+        this.customerId = customerId ;
     }
-
-    public String getName() {
-        return name;
+    
+    
+    public String getName(){
+        return name ;
     }
-
-    public void setName(String name) {
-        this.name = name;
+     
+     public void setName(String name) {
+        this.email = email ;
+     }
+     
+     
+    public String getEmail(){
+        return email ;
     }
-
-    public String getEmail() {
-        return email;
-    }
-
     public void setEmail(String email) {
-        this.email = email;
-    }
+        this.email = email ;
+     }
     
-    public LinkedList<Integer> getOrders ()
-    {
-       return orders;
+    public LinkedList<Integer> getOrders(){
+        return orders ;
     }
-    
-    public void addOrder ( Integer order)
-    {
-        orders.findFirst();
+    public void addOrder (Integer order){
         orders.insert(order);
     }
-    
-    
-      public boolean removeOrder( Integer R)
-    {
-        if ( ! orders.empty())
-        {
+    public boolean removeOrder(Integer R){
+        if(!orders.empty()){
             orders.findFirst();
-            while(! orders.last())
+            while(orders.last())
             {
-                if (orders.retrieve().equals(R))
-                {
+                if (orders.retrieve() == R){
                     orders.remove();
-                    return true;
+                    return true ;
                 }
-                
+                else
                     orders.findNext();
+                   
             }
-            if (orders.retrieve().equals(R))
-            {
-                orders.remove();
-                return true;
+            if (orders.retrieve() == R){
+                    orders.remove();
+                    return true ;
             }
         }
-        return false;
+        return false ;
     }
     
-    
-    @Override
-public String toString() {
-    String str = "\n----------------------------" +
-                 "\nCustomer ID: " + customerId +
-                 "\nName: " + name +
-                 "\nEmail: " + email;
-
-    if (!orders.empty()) {
-        str += "\nOrders: [ ";
-        orders.findFirst();
-        while (!orders.last()) {
-            str += orders.retrieve() + " | ";
-            orders.findNext();
+    public String toString() {
+        String str = "\n Customers { " + "customer ID : " +customerId + " , name : " + name + " , email : " + email ;
+        if (! orders.empty()){
+            str += "(order List" ;
+            orders.findFirst();
+            while(orders.last()){
+                str += orders.retrieve() + " " ;
+                orders.findNext();
+            }
+            str += orders.retrieve() + " )" ;
         }
-        str += orders.retrieve() + " ]";
+        str += " }" ;
+        return str ;
     }
-    return str;
+    
 }
-
-}
-
