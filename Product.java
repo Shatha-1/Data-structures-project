1972f1105592197bcba462a338eb650bcd49e29d
Shatha-1 <shatha.14297@gmail.com>
2025-11-06 17:58:31 +0300

    Add files via upload

--- Product.java
+++ Product.java
@@ -1,101 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package csc212;
 
-/**
- *
- * @author Manal Alhihi
- */
-public class Product {
-    private int productId;
+public class Product{
+    private int ProductId;
     private String name;
     private double price;
     private int stock;
     private LinkedList <Integer> reviews = new LinkedList <Integer> ();
-
-    public Product() {
-        this.productId = 0;
-        this.name = "";
-        this.price = 0;
-        this.stock = 0;
-    }
-
-    public Product(int productId, String name, double price, int stock) {
-        this.productId = productId;
-        this.name = name;
-        this.price = price;
-        this.stock = stock;
-    }
-
-    // ===== Getters and Setters =====
-    public int getProductId() { return productId; }
-    public String getName() { return name; }
-    public double getPrice() { return price; }
-    public int getStock() { return stock; }
-
-    public void setName(String name) { this.name = name; }
-    public void setPrice(double price) { this.price = price; }
-    public void setStock(int stock) { this.stock = stock; }
-
-    public void addStock ( int stock)
-    {
-        this.stock += stock;
-    }
     
-    public void removeStock ( int stock)
-    {
-        this.stock -= stock;
-        if (this.stock < 0) this.stock = 0;
-    }
-    
-    
-    public void addReview( Integer R)
-    {
-        reviews.insert(R);
-    }
-    
-    public boolean removeReview( Integer R)
-    {
-        if ( ! reviews.empty())
+public Product(){
+    this.ProductId=0;
+    this.name="";
+    this.price=0;
+    this.stock=0;
+}
+public Product(int ProductId ,String name , double price , int stock){
+    this.ProductId=ProductId;
+    this.name=name;
+    this.price=price;
+    this.stock=stock;
+}
+
+public int getProductId() { 
+    return ProductId ;
+}
+public String getName() { 
+    return name ;
+}
+public double getPrice() { 
+    return price ;
+}
+public int getStock() { 
+    return stock ;
+}
+
+public void setName(String name){ 
+    this.name=name ;
+}
+
+public void setPrice(double price){ 
+    this.price=price ;
+}
+
+public void setStock(int stock){ 
+    this.stock=stock ;
+}
+
+public void addStock(int stock){
+    this.stock += stock;
+}
+
+public void removeStock (int stock){
+    this.stock -= stock;
+    if (this.stock < 0) this.stock= 0;
+}
+
+public void addReview(Integer Review){
+    reviews.insert(Review);
+}
+
+public boolean removeReview(Integer Review){
+    if (!reviews.empty()){
+        reviews.findFirst();
+        while (!reviews.last())
         {
-            reviews.findFirst();
-            while(!reviews.last())
-            {
-                if (reviews.retrieve().equals(R))
-                {
-                    reviews.remove();
-                    return true;
-                }
-                
-                    reviews.findNext();
-            }
-            if (reviews.retrieve().equals(R))
+            if(reviews.retrieve().equals(Review))
             {
                 reviews.remove();
                 return true;
             }
+            reviews.findNext();
         }
-        return false;
-    }
-    
-    public LinkedList<Integer> getReviews ()
-    {
-        return reviews;
-    }
-    
-    @Override
-    public String toString() {
-        String str =  "\nProduct ID:" + productId + "\nName: " + name + "\nPrice: " + price + "\nStock: " + stock ;
-        if ( ! reviews.empty())
-        {
-            str += "\nReviews: [ " ;
-            reviews.findFirst();
-            while(! reviews.last())
+        if(reviews.retrieve().equals(Review))
             {
-                str += reviews.retrieve() + " | ";
-                reviews.findNext();
+                reviews.remove();
+                return true;
             }
-            str += reviews.retrieve() + " ]";
-        }
-        return str;        
     }
-    
+    return false;
+}// end removeReview
+
+public String toString(){
+    String str = "\n Product ID :" + ProductId + "\n Name :" + name +  "\n price :" + price + "\n stock :" + stock ;
+    if (!reviews.empty()){
+        str += "\n Review :[";
+        reviews.findFirst();
+        while (!reviews.last())
+        {
+            str += reviews.retrieve() + "|" ;
+            reviews.findNext();
+        }
+        str += reviews.retrieve() + "]" ;
+    }
+    return str;
+}
 }
--- Review.java
+++ Review.java
@@ -1,49 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package csc212;
+
 
 public class Review {
-    private int reviewId;
-    private int productId;
-    private int customerId;
-    private int rating;
-    private String comment;
+int reviewId;
+int productID;
+int customerID;
+int rating;
+String comment;
 
-    public Review() {    
-    this.reviewId = 0;    
-    this.productId = 0;    
-    this.customerId = 0;    
-    this.rating = 0;    
-    this.comment = "";    
+public Review(){
+    this.reviewId=0;
+    this.productID=0;
+    this.customerID=0; 
+    this.rating=0;
+    this.comment="";
 }
-    public Review(int reviewId, int productId, int customerId, int rating, String comment) {
-        this.reviewId = reviewId;
-        this.productId = productId;
-        this.customerId = customerId;
-        setRating(rating); //   
-        this.comment = (comment == null) ? "" : comment;
-    }
 
-    public int getReviewId() { return reviewId; }
-    public int getProductId() { return productId; }
-    public int getCustomerId() { return customerId; }
-    public int getRating() { return rating; }
-    public String getComment() { return comment; }
+public Review(int reviewId ,int productID,int customerID, int rating, String comment ){
+    this.reviewId=reviewId;
+    this.productID=productID;
+    this.customerID=customerID; 
+    this.rating=rating;
+    this.comment=comment;
+}
+public int getReviewId() { 
+    return reviewId ;
+}
+public int getProduct() { 
+    return productID ;
+}
+public int getCustomer() { 
+    return customerID ;
+}
+public int getRating() { 
+    return rating ;
+}
+public String getComment(){
+    return comment;
+}
 
-    public void setRating(int rating) {
-        if (rating < 1 || rating > 5) {
-            throw new IllegalArgumentException("Rating must be 1..5");
-        }
-        this.rating = rating;
-    }
+public void setReviewId(int reviewId){
+    this.reviewId=reviewId;
+}
+public void setProduct(int productID){
+    this.productID=productID;
+}
+public void setCustomer(int customerID){
+    this.customerID=customerID;
+}
 
-    public void setComment(String comment) {
-        this.comment = (comment == null) ? "" : comment;
-    }
-
-    @Override
-    public String toString() {
-        return "Review{id=" + reviewId +
-                ", product=" + productId +
-                ", customer=" + customerId +
-                ", rating=" + rating +
-                ", comment='" + comment + "'}";
-    }
+public void setRating(int rating){
+    this.rating=rating;
+}
+public void setComment(String comment){
+    this.comment=comment;
+}
+public String toString(){
+    return "\n Review{" + " reviewId= "+reviewId + " productID= "+ productID+ " customerID= " + customerID+ " rating= "+ rating+ " comment = " + comment + "}";
+}
 }
